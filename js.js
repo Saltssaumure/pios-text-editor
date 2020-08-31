3e3eed1306be44d660e6ff6a3d3cafaae4118c2c
Saltssaumure <randomized.goldfish@gmail.com>
2020-09-01 00:50:42 +0100

    Upload all

---
+++ js.js
@@ -0,0 +1,9 @@
+function help() {
+    var ok = document.getElementById("helpful");
+    if (ok.style.display === "none") {
+        ok.style.display = "block";
+    }
+    else {
+        ok.style.display = "none";
+    }
+}
